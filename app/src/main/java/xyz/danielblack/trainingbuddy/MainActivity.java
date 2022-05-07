d77354c61e94d05269a3437fbbe4ca26f8520947
Daniel Fekete <daniel.fekete97@gmail.com>
2022-05-07 22:26:59 +0200

    Added landscape mode for login
    Closes #7

--- app/src/main/java/xyz/danielblack/trainingbuddy/MainActivity.java
+++ app/src/main/java/xyz/danielblack/trainingbuddy/MainActivity.java
@@ -3,12 +3,23 @@ package xyz.danielblack.trainingbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        EditText email = findViewById(R.id.editTextTextEmailAddress);
+        EditText password = findViewById(R.id.editTextTextPassword);
+
+        Log.i(LOG_TAG, email.getText().toString() + " => " + password.getText().toString());
     }
 }
