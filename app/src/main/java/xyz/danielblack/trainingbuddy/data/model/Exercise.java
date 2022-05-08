fc648a6d93bf89b4f665ea91b8b906f02da68f83
Daniel Fekete <daniel.fekete97@gmail.com>
2022-05-08 15:21:33 +0200

    Created basic User data model
    Closes #15

--- app/src/main/java/xyz/danielblack/trainingbuddy/data/model/Exercise.java
+++ app/src/main/java/xyz/danielblack/trainingbuddy/data/model/Exercise.java
@@ -1,7 +1,7 @@
 package xyz.danielblack.trainingbuddy.data.model;
 
 public class Exercise {
-    private String name;
-    private String category; // Barbell, Dumbbell, Machine/Other, Weighted bodyweight, Reps only, Cardio exercise, Duration
-    private String bodyPartTargeted; // None, Core, Arms, Back, Chest, Legs, Shoulders, Other, Olympic, Full Body, Cardio
+    private String mName;
+    private String mCategory; // Barbell, Dumbbell, Machine/Other, Weighted bodyweight, Reps only, Cardio exercise, Duration
+    private String mBodyPartTargeted; // None, Core, Arms, Back, Chest, Legs, Shoulders, Other, Olympic, Full Body, Cardio
 }
--- app/src/main/java/xyz/danielblack/trainingbuddy/data/model/User.java
+++ app/src/main/java/xyz/danielblack/trainingbuddy/data/model/User.java
@@ -1,4 +1,6 @@
 package xyz.danielblack.trainingbuddy.data.model;
 
 public class User {
+    private String mUsername;
+    private int mTotalNumberOfWorkouts;
 }
